71da031a4f97d2465c8dba95971cb51e3f9fede4
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-11-07 17:50:20 +0900

    [ADD] input
    input 후 출력 기능

--- text_similarity/src/App.js
+++ text_similarity/src/App.js
@@ -1,19 +1,36 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleAdd = () => {
+    setItems([...items, inputText]);
+    setInputText("");
+  };
+
   return (
     <div className="App">
       <h3>꼬꼬단 - 꼬리의 꼬리를 무는 단어</h3>
       오늘의 꼬꼬단 영단어를 맞혀보세요.
-      <form>
-        <div className="guess-wrapper">
-          <input placeholder="추측할 단어를 입력하세요" />
-          <input type="submit" value="추측" id="guess-btn" />
-        </div>
-      </form>
-      <div>
+      <div className="guess-wrapper">
+        <input
+          type="text"
+          name="inputText"
+          placeholder="추측할 단어를 입력하세요"
+          value={inputText}
+          onChange={handleInputChange}
+        />
+        <input type="submit" value="추측" id="guess-btn" onClick={handleAdd} />
+      </div>
+      <div className="table-container">
         <table>
-          <tbody>
+          <thead>
             <tr>
               <th>횟수</th>
               <th>단어</th>
@@ -23,16 +40,20 @@ function App() {
             </tr>
             <tr>
               <td>
-                <hr></hr>
+                <hr className="table-hr" />
               </td>
             </tr>
-            <tr>
-              <td>횟수</td>
-              <td>단어</td>
-              <td>뜻</td>
-              <td>유사도</td>
-              <td>순위</td>
-            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item}</td>
+                <td>뜻</td>
+                <td>유사도</td>
+                <td>순위</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
