8c8dbcffe00105fe18f3417c1845f10edac4afe4
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-11-07 14:11:43 +0900

    project 생성

---
+++ text_similarity/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>오늘의 단어</h3>
+      <input />
+      <button>등록</button>
+      <div>유사도</div>
+    </div>
+  );
+}
+
+export default App;
