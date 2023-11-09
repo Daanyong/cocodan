2923089255a4e21f236362c11366da97539908f6
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-11-09 14:53:36 +0900

    [ADD] data 받아오기
    뜻, 유사도 받아와서 출력

--- text_similarity/src/App.js
+++ text_similarity/src/App.js
@@ -5,35 +5,54 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
+  // 입력받은 값 설정
   const handleInputChange = (e) => {
     setInputText(e.target.value);
   };
 
-  const handleAdd = (e) => {
+  const handleAdd = async (e) => {
     e.preventDefault(); // 폼 제출 x
-    setItems([...items, inputText]);
-    setInputText("");
 
-    //// 이 부분은 from 데이터로 전송해야 flask가 파싱할 수 있어서 주석처리하고 새로 짯어요!
-    const Data = new FormData(document.querySelector("form"));
+    // from 데이터 전송
+    const Data = new FormData();
     Data.append("inputText", inputText);
-    // 전달
-    fetch("http://shogle.site:10021/result", {
+
+    const response = await fetch("http://shogle.site:10021/result", {
       method: "POST",
-      //body: Data,
-      headers: {
-        //Origin: "http://localhost:8080/",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        inputText: Data.get("inputText"),
-      }),
-    }).then((res) => res.json());
+      body: Data,
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      // 전달 잘 됐을 때 저장
+      setItems([...items, data]);
+      setInputText("");
+    } else {
+      console.error("Failed to fetch data");
+    }
   };
 
   const handleInvalidInput = (e) => {
     e.target.setCustomValidity("영문자만 입력 가능해요!");
   };
+
+  // API를 호출, 데이터 받아옴
+  function fetchData() {
+    const data = new URLSearchParams();
+    data.append("inputText", inputText);
+
+    fetch("http://shogle.site:10021/", {
+      method: "POST",
+      body: data,
+    })
+      .then((response) => response.json()) // JSON 형식으로 변환
+      .then((data) => {
+        console.log(data); // 데이터 확인용
+      })
+      .catch((error) => console.error("Error:", error));
+  }
+
+  fetchData();
 
   return (
     // 시간이 있을진 모르겠지만 아래 부분엔 페이지를 위한 간략한 설명 부분도 추가되어 있음 좋을 거 같습니다.
@@ -77,9 +96,9 @@ function App() {
             <tbody>
               <tr className="recent">
                 <td>{items.length}</td>
-                <td>{items[items.length - 1]}</td>
-                <td>뜻</td>
-                <td>유사도</td>
+                <td>{items[items.length - 1].inputText}</td>
+                <td>{items[items.length - 1].translated}</td>
+                <td>{items[items.length - 1].simlity}</td>
                 <td>순위</td>
               </tr>
             </tbody>
@@ -94,9 +113,9 @@ function App() {
             {items.map((item, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
-                <td>{item}</td>
-                <td>뜻</td>
-                <td>유사도</td>
+                <td>{item.inputText}</td>
+                <td>{item.translated}</td>
+                <td>{item.simlity}</td>
                 <td>순위</td>
               </tr>
             ))}
