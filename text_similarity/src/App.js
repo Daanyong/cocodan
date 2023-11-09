82e701aafcf7967ce27e46e2da1c1b65a8f4c500
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-11-09 14:23:19 +0900

    [FIX] 정규화
    길이 제한, 영어만 가능

--- text_similarity/src/App.js
+++ text_similarity/src/App.js
@@ -13,28 +13,29 @@ function App() {
     e.preventDefault(); // 폼 제출 x
     setItems([...items, inputText]);
     setInputText("");
-    
+
     //// 이 부분은 from 데이터로 전송해야 flask가 파싱할 수 있어서 주석처리하고 새로 짯어요!
-    const Data = new FormData();
+    const Data = new FormData(document.querySelector("form"));
     Data.append("inputText", inputText);
     // 전달
-    // 굿 감사합니다.. 프론트는 힘들더라고여..대다네..
     fetch("http://shogle.site:10021/result", {
       method: "POST",
-      body: Data
-      // headers: {
-      //   Origin: "http://localhost:8080/",
-      //   "Content-Type": "application/json",
-      // },
-      // body: JSON.stringify({
-      //   inputText: inputText,
-      // }),
+      //body: Data,
+      headers: {
+        //Origin: "http://localhost:8080/",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        inputText: Data.get("inputText"),
+      }),
     }).then((res) => res.json());
   };
 
+  const handleInvalidInput = (e) => {
+    e.target.setCustomValidity("영문자만 입력 가능해요!");
+  };
+
   return (
-    // 입력될 때, 클라이언트 쪽에서 영어만 입력 가능하게 정규화되어 있으면 좋겠어요
-    // 또, 길이 제한이 있으면 좋겠어요 한 번 입력에 한 20-25개 글자 이상은 입력이 안 될 수 있도록
     // 시간이 있을진 모르겠지만 아래 부분엔 페이지를 위한 간략한 설명 부분도 추가되어 있음 좋을 거 같습니다.
     <div className="App">
       <h3>꼬꼬단 - 꼬리의 꼬리를 무는 단어</h3>
@@ -45,9 +46,13 @@ function App() {
             type="text"
             name="inputText"
             className="inputText"
-            placeholder="추측할 단어를 입력하세요" 
+            placeholder="추측할 단어를 입력하세요"
             value={inputText}
             onChange={handleInputChange}
+            pattern="[A-Za-z]+"
+            maxLength={20}
+            onInvalid={handleInvalidInput}
+            required
           />
           <input
             type="submit"
